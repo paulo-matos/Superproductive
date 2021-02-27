bf3b1d2f72d9c4beacf65d26b1b77c2d51903e4d
Paulo Matos <contato.paulomatos@gmail.com>
2021-02-27 19:47:44 -0300

    All challenges translated. Reset Countdown function now reseting to 25 minutes.

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -36,7 +36,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
         clearTimeout(countdownTimeout); //clears the timeout event - pure JS - so it REALLY stops the timer
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.1 * 60);
+        setTime(25 * 60);
     }
 
     useEffect(() => { //generates "collateral effects"
